21adc65f902b213a13145dc8a260fd95d47fcdfb
Hash <kxhashimoto@gmail.com>
2020-05-07 17:14:12 +0200

    Add files via upload

---
+++ remake/funcs/game/fov.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TestMod.remake.funcs.game
+{
+    public class fov
+    {
+        public static void set_cam_fov(float v)
+        {
+            var gameObject = GameObject.Find("Camera (eye)");
+            if (gameObject != null)
+            {
+                var component = gameObject.GetComponent<Camera>();
+                if (component != null) component.fieldOfView = v;                
+            }
+        }
+    }
+}
---
+++ remake/funcs/game/speed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMod.remake.funcs.game
+{
+    public class speed
+    {
+        public static void set_speeds(float walk, float run)
+        {
+            var locomotion = VRCPlayer.field_Internal_Static_VRCPlayer_0.GetComponent<LocomotionInputController>();
+            if (locomotion != null)
+            {
+                /*speeds*/
+                locomotion.runSpeed = run;
+                locomotion.walkSpeed = walk;
+            }
+        }
+    }
+}
